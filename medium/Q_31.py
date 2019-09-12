32a1e7be86019e52ee19126456097c83e675dc69
chenkang <ckanggongc0421@163.com>
2019-09-12 22:09:43 +0800

    ck

--- medium/Q_31.py
+++ medium/Q_31.py
@@ -15,3 +15,42 @@
     3,2,1 → 1,2,3
     1,1,5 → 1,5,1
 """
+from typing import List
+
+
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> List[int]:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        firstIndex = -1
+        n = len(nums)
+
+        def reverse(nums, i, j):
+            while i < j:
+                nums[i], nums[j] = nums[j], nums[i]
+                i += 1
+                j -= 1
+
+        for i in range(n - 2, -1, -1):
+            if nums[i] < nums[i + 1]:
+                firstIndex = i
+                break
+        # print(firstIndex)
+        if firstIndex == -1:
+            reverse(nums, 0, n - 1)
+            return
+        secondIndex = -1
+        for i in range(n - 1, firstIndex, -1):
+            if nums[i] > nums[firstIndex]:
+                secondIndex = i
+                break
+        nums[firstIndex], nums[secondIndex] = nums[secondIndex], nums[firstIndex]
+        reverse(nums, firstIndex + 1, n - 1)
+        return nums
+
+
+if __name__ == "__main__":
+    s = Solution()
+    results = s.nextPermutation([3, 2, 1])
+    print(results)
